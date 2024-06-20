c82d2c73999f3f1b2157d3134799e06e10004727
Justramin <143264759+Justramin@users.noreply.github.com>
2024-06-20 18:40:37 +0530

    admin login proper error messaging.

--- controllers/adminControllers/auth/admin-auth.js
+++ controllers/adminControllers/auth/admin-auth.js
@@ -16,27 +16,39 @@ const admin_login = async(req,res)=>{
 
 
 
-const admin_login_post = async(req,res)=>{
+const admin_login_post = async (req, res) => {
     try {
-        const adminData= req.body
-        const findData =await adminCollection.findOne({email:adminData.email})
-        if(!findData || findData.password !== adminData.password || !findData.status){
-            console.log("wrong somthing");
-            req.flash('error', '*You are Blocked by Super Admin')
-            res.redirect('/admin')
-        }else {
-            if(findData.superAdmin){
-                req.session.isSuperAdmin=true
-            }
-            req.session.isAdminAuth = true
-            req.session.adminEmail = req.body.email
-            return res.redirect('/admin/admin_dashbord')
+        const adminData = req.body;
+        const findData = await adminCollection.findOne({ email: adminData.email });
+
+        if (!findData) {
+            req.flash('error', '*Please enter correct email and password');
+            return res.redirect('/admin');
         }
+
+        if (findData.password !== adminData.password) {
+            req.flash('error', '*Incorrect password');
+            return res.redirect('/admin');
+        }
+
+        if (!findData.status) {
+            req.flash('error', '*Access denied. Try again later');
+            return res.redirect('/admin');
+        }
+
+        if (findData.superAdmin) {
+            req.session.isSuperAdmin = true;
+        }
+
+        req.session.isAdminAuth = true;
+        req.session.adminEmail = adminData.email;
+        return res.redirect('/admin/admin_dashbord');
     } catch (error) {
         console.error('Error in admin_login_post:', error);
-        res.redirect('/admin/errorPage')
-    }  
-}
+        res.redirect('/admin/errorPage');
+    }
+};
+
 
 
 const admin_logout = async(req,res)=>{
